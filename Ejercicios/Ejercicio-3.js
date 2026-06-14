17d46d29083f8b1e7a9d601af1a5c8df1a0e4583
Luis-Lopez-Pineda <luisfernandolopezpineda21@gmail.com>
2026-06-13 21:15:09 -0600

    "Ejercicio 3 Listo"

--- Ejercicios/Ejercicio-3.js
+++ Ejercicios/Ejercicio-3.js
@@ -1,2 +1,33 @@
 //Ejercicio 3: Uso de async y await para Manejar Promesas
 
+const consultaBD = async () =>{
+
+    return new Promise((resolve, reject) => {
+        setTimeout( () => {
+
+            const numRandom = Math.floor(Math.random() * 10000000)
+            console.log(numRandom)
+            if (numRandom % 2 === 0){
+                resolve('Consulta exitosa')
+            }
+            else{
+                reject('Error en la consulta')
+            }
+            
+        }, 3000);
+    })
+}
+
+const HacerConsulta = async  () => {
+
+    try{
+        const consulta = await consultaBD()
+        console.log(consulta)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+console.log(HacerConsulta())
+   
