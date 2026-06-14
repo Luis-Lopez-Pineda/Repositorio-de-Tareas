0a7233782d6c0330ea2fb5acd90f6ba64124d75b
Luis-Lopez-Pineda <luisfernandolopezpineda21@gmail.com>
2026-06-13 21:00:04 -0600

    "Ejercicio 2 Listo"

---
+++ Ejercicios/Ejercicio-2.js
@@ -0,0 +1,29 @@
+//Ejercicio 2: Simulación de una Operación Asíncrona con Promesas 
+
+const consultaBD = async () =>{
+
+    return new Promise((resolve, reject) => {
+        setTimeout( () => {
+
+            const numRandom = Math.floor(Math.random() * 10000000)
+            console.log(numRandom)
+            if (numRandom % 2 === 0){
+                resolve('Consulta exitosa')
+            }
+            else{
+                reject('Error en la consulta')
+            }
+            
+        }, 3000);
+    })
+}
+
+const basededatos = consultaBD()
+.then( (response ) => {
+    console.log(response)
+})
+.catch( (error) => {
+    console.log(error)
+})
+
+console.log(basededatos)
