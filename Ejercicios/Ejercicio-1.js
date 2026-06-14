65784d84fe97a3f6433d8fc0b84426bc9dd98497
Luis-Lopez-Pineda <luisfernandolopezpineda21@gmail.com>
2026-06-13 20:38:07 -0600

    "Ejercicio 1 Listo"

--- Ejercicios/Ejercicio-1.js
+++ Ejercicios/Ejercicio-1.js
@@ -1,1 +1,20 @@
 //Ejercicio 1: Consumo de una API y Procesamiento de Datos
+const getUsers = async () => {
+
+        try{
+
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            if(!response.ok) return
+
+            const users = await response.json()
+            users.forEach((user) => {
+                console.log('Nombre:', user.name, 'Email:', user.email)
+            })            
+            return users
+            }
+            catch(erorr){
+            console.log('Hubo un error')
+        }
+}
+
+getUsers();
